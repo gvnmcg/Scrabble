e3d1cc728a86d00f546e1f83a94713cb0c7ecd55
Gavin <gvnmcg517@gmail.com>
2019-03-03 17:11:19 -0700

    -controller class

---
+++ src/Controller.java
@@ -0,0 +1,38 @@
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Controller {
+
+    private Manager manager;
+
+    Controller(Manager manager){
+        this.manager = manager;
+    }
+
+
+    EventHandler<MouseEvent> handleLetterSelection(Letter l){
+
+        return new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                manager.selectLetter(l);
+
+                System.out.println(l);
+            }
+        };
+    }
+
+    EventHandler<MouseEvent> handleSpaceClick(BoardSpace bs) {
+        return new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                //TODO
+                System.out.println("handle ");
+
+                manager.selectBoardSpace(bs);
+            }
+        };
+    }
+}
